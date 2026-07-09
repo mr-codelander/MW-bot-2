5daa82488d24b7e6804aaca3f3624ec0e532d75f
mr-codelander <ali2025ad92@gmail.com>
2026-07-09 10:55:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -566,7 +566,7 @@ async def start(bot, message):
 
         ACTIVE_GROUPS[message.chat_id] = True
         save_active_groups(ACTIVE_GROUPS)
-		await message.reply(WELCOME_MESSAGE)
+        await message.reply(WELCOME_MESSAGE)
 	else:
 		await message.reply_keypad(
             A_MESSAGE,
